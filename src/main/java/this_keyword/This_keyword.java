784274012a8b2f60f13f21f5abf8d1865819484f
offic <offic@DESKTOP-GLF4UNU>
2024-07-08 09:46:16 +0600

    Introduction to "this" keyword.

---
+++ src/main/java/this_keyword/This_keyword.java
@@ -0,0 +1,8 @@
+package this_keyword;
+
+public class This_keyword {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"this keyword\".");
+    }
+}
