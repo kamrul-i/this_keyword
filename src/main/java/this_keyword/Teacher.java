b03d1c6d066176cd665c690bc2d044419acff210
offic <offic@DESKTOP-GLF4UNU>
2024-07-08 11:30:59 +0600

    How to refer current class instance variable ?

--- src/main/java/this_keyword/Teacher.java
+++ src/main/java/this_keyword/Teacher.java
@@ -6,15 +6,15 @@ public class Teacher {
     int age, phone;
 
     Teacher(String name, String subjectName, int age) {
-        name = name;
-        subjectName = subjectName;
-        age = age;
+        this.name = name;
+        this.subjectName = subjectName;
+        this.age = age;
     }
 
     void display() {
-        System.out.println("Name : " + name);
+        System.out.println("Name        : " + name);
         System.out.println("SubjectName : " + subjectName);
-        System.out.println("Age : " + age);
+        System.out.println("Age         : " + age);
     }
 
     /*
@@ -26,6 +26,14 @@ public class Teacher {
     
     একত্রে পেলে, java instance variable এর তুলনায় local variable-কে বেশি গুরুত্ব দেয়, এবং এখানে
     উভয় পক্ষকে local variable হিসেবে ধরে নেয়, আর এটাকে instance variable hiding বলে।।
-    variable hiding এর কারণে উপরে error দেখাচ্ছে, পরবর্তী commit-এ সমাধান।-----------
+    variable hiding এর কারণে উপরে error দেখাচ্ছে, 
+    
+    এ error এর সমাধান হচ্ছে, "this" keyword
+    "this" keyword এর সাহায্যে current class-এর object-কে refer করা হয়।।
+    
+    Usages of "this" keyword :
+    refer current class instance variable.
+    it can be used to call current class constructor.
+    it can be used to call current class method.
      */
 }
--- src/main/java/this_keyword/This_keyword.java
+++ src/main/java/this_keyword/This_keyword.java
@@ -1,7 +1,5 @@
 package this_keyword;
-
 public class This_keyword {
-
     public static void main(String[] args) {
 
         Teacher teacher = new Teacher("Kamrul Islam", "Science", 31);
