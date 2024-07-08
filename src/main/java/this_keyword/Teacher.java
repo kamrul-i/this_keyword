fb284623747822abeb6c3906c2d467b7547c4075
offic <offic@DESKTOP-GLF4UNU>
2024-07-08 11:53:03 +0600

    How to call current class constructor ?

--- src/main/java/this_keyword/Teacher.java
+++ src/main/java/this_keyword/Teacher.java
@@ -11,24 +11,23 @@ public class Teacher {
         this.age = age;
     }
 
+    Teacher(String name, String subjectName, String qualification, int age, int phone) {
+        this(name, subjectName, age);     //উপরের constructor-কে call করা হয়েছে,
+        this.qualification = qualification;
+        this.phone = phone;
+    }
+
     void display() {
-        System.out.println("Name        : " + name);
-        System.out.println("SubjectName : " + subjectName);
-        System.out.println("Age         : " + age);
+        System.out.println("Name          : " + name);
+        System.out.println("SubjectName   : " + subjectName);
+        System.out.println("Qualification : " + qualification);
+        System.out.println("Age           : " + age);
+        System.out.println("Phone         : " + phone);
+        System.out.printf("\n");
     }
 
     /*
-    Instance variable hiding :
-    Instance variable & local variable same ধরা হয়েছে, 
-    যেমন- name = name;
-         subjectName = subjectName;
-         age = age;
-    
-    একত্রে পেলে, java instance variable এর তুলনায় local variable-কে বেশি গুরুত্ব দেয়, এবং এখানে
-    উভয় পক্ষকে local variable হিসেবে ধরে নেয়, আর এটাকে instance variable hiding বলে।।
-    variable hiding এর কারণে উপরে error দেখাচ্ছে, 
-    
-    এ error এর সমাধান হচ্ছে, "this" keyword
+    "this" keyword মূলত object oriented programming_OOP এর অংশ।। 
     "this" keyword এর সাহায্যে current class-এর object-কে refer করা হয়।।
     
     Usages of "this" keyword :
--- src/main/java/this_keyword/This_keyword.java
+++ src/main/java/this_keyword/This_keyword.java
@@ -1,8 +1,13 @@
 package this_keyword;
+
 public class This_keyword {
+
     public static void main(String[] args) {
 
-        Teacher teacher = new Teacher("Kamrul Islam", "Science", 31);
-        teacher.display();
+        Teacher teacher1 = new Teacher("Kamrul Islam", "Science", 31);
+        teacher1.display();
+
+        Teacher teacher2 = new Teacher("Altaf Hussain", "Computer", "java expert", 34, 17236291);
+        teacher2.display();
     }
 }
